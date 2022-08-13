ea14596dc3d0dbcceb594346aefe0b967d1b0632
Ilya Sudakov <ilyasudakov@inbox.ru>
2022-08-13 11:47:02 +0300

    Add Redux Persist to save stack state on reload

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { githubAPI } from './github/github.api';
 import stackSlice from '../components/Stack/stackSlice';
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  blacklist: [githubAPI.reducerPath],
+};
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({ [githubAPI.reducerPath]: githubAPI.reducer, stackSlice })
+);
+
 const store = configureStore({
-  reducer: {
-    [githubAPI.reducerPath]: githubAPI.reducer,
-    stackSlice,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch: () => AppDispatch = useDispatch;
